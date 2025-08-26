2f139f7add6f0dcc1326afdb4eef6ae16c0f3468
Paul Mann <paulmann2205@gmail.com>
2025-08-26 10:56:16 +0200

    Implemented eager loading

--- Game.py
+++ Game.py
@@ -29,204 +29,261 @@ class Game:
     platforms: list[str] = field(default_factory=list)
     cover: Optional[str] = None
 
-    _cache: dict = field(default_factory=dict, init=False,repr=False)
+    cache: dict = field(default_factory=dict,repr=False)
 
     @property
     def aggregated_rating(self):
+        """aggregated_rating"""
         return self._lazy_load('aggregated_rating')
 
     @property
     def aggregated_rating_count(self):
+        """aggregated_rating_count"""
         return self._lazy_load('aggregated_rating_count')
 
     @property
     def alternative_names(self):
+        """alternative_names.name"""
         return self._lazy_load('alternative_names.name')
 
     @property
     def artworks(self):
+        """artworks.url"""
         return [('https:' + url).replace('t_thumb','t_1080p') for url in self._lazy_load('artworks.url')]
 
     @property
     def bundles(self):
+        """bundles.name"""
         return self._lazy_load('bundles.name')
 
     @property
     def collections(self):
+        """collections.name"""
         return self._lazy_load('collections.name')
 
     @property
     def dlcs(self):
+        """dlcs.name"""
         return self._lazy_load('dlcs.name')
 
     @property
     def expanded_games(self):
+        """expanded_games.name"""
         return self._lazy_load('expanded_games.name')
 
     @property
     def expansions(self):
+        """expansions.name"""
         return self._lazy_load('expansions.name')
 
     @property
     def first_release_date(self):
+        """first_release_date"""
         if self._lazy_load('first_release_date'):
             return datetime.datetime.fromtimestamp(self._lazy_load('first_release_date'), datetime.UTC)
         return None
 
     @property
     def forks(self):
+        """forks.name"""
         return self._lazy_load('forks.name')
 
     @property
     def franchise(self):
+        """franchise.name"""
         return self._lazy_load('franchise.name')
 
     @property
     def franchises(self):
+        """franchises.name"""
         return self._lazy_load('franchises.name')
 
     @property
     def game_engines(self):
+        """game_engines.name"""
         return self._lazy_load('game_engines.name')
 
     @property
     def game_localizations(self):
+        """game_localizations.name"""
         return self._lazy_load('game_localizations.name')
 
     @property
     def game_modes(self):
+        """game_modes.name"""
         return self._lazy_load('game_modes.name')
 
     @property
     def game_status(self):
+        """game_status.status"""
         return self._lazy_load('game_status.status')
 
     @property
     def game_type(self):
+        """game_type.type"""
         return self._lazy_load('game_type.type')
 
     @property
     def genres(self):
+        """genres.name"""
         return self._lazy_load('genres.name')
 
     @property
     def hypes(self):
+        """hypes"""
         return self._lazy_load('hypes')
 
     @property
     def involved_companies(self):
+        """involved_companies.company.name"""
         return self._lazy_load('involved_companies.company.name')
 
     @property
     def keywords(self):
+        """keywords.name"""
         return self._lazy_load('keywords.name')
 
     @property
     def language_supports(self):
+        """language_supports.language.name"""
         return self._lazy_load('language_supports.language.name')
 
     @property
     def multiplayer_modes(self):
+        """multiplayer_modes"""
         return self._lazy_load('multiplayer_modes')
 
     @property
     def parent_game(self):
+        """parent_game.name"""
         return self._lazy_load('parent_game.name')
 
     @property
     def player_perspectives(self):
+        """player_perspectives.name"""
         return self._lazy_load('player_perspectives.name')
 
     @property
     def ports(self):
+        """ports.name"""
         return self._lazy_load('ports.name')
 
     @property
     def rating(self):
+        """rating"""
         return self._lazy_load('rating')
 
     @property
     def rating_count(self):
+        """rating_count"""
         return self._lazy_load('rating_count')
 
     @property
     def release_dates(self):
+        """release_dates.date"""
         return self._lazy_load('release_dates.date')
 
     @property
     def remakes(self):
+        """remakes.name"""
         return self._lazy_load('remakes.name')
 
     @property
     def remasters(self):
+        """remasters.name"""
         return self._lazy_load('remasters.name')
 
     @property
     def screenshots(self):
+        """screenshots.url"""
         return [('https:' + url).replace('t_thumb','t_1080p') for url in self._lazy_load('screenshots.url')]
 
     @property
     def similar_games(self):
+        """similar_games.name"""
         return self._lazy_load('similar_games.name')
 
     @property
     def slug(self):
+        """slug"""
         return self._lazy_load('slug')
 
     @property
     def standalone_expansions(self):
+        """standalone_expansions.name"""
         return self._lazy_load('standalone_expansions.name')
 
     @property
     def storyline(self):
+        """storyline"""
         return self._lazy_load('storyline')
 
     @property
     def summary(self):
+        """summary"""
         return self._lazy_load('summary')
 
     @property
     def tags(self):
+        """tags"""
         return self._lazy_load('tags')
 
     @property
     def themes(self):
+        """themes.name"""
         return self._lazy_load('themes.name')
 
     @property
     def total_rating(self):
+        """total_rating"""
         return self._lazy_load('total_rating')
 
     @property
     def total_rating_count(self):
+        """total_rating_count"""
         return self._lazy_load('total_rating_count')
 
     @property
     def version_parent(self):
+        """version_parent.name"""
         return self._lazy_load('version_parent.name')
 
     @property
     def version_title(self):
+        """version_title"""
         return self._lazy_load('version_title')
 
     @property
     def videos(self):
+        """videos.video_id"""
         videos = self._lazy_load('videos.video_id')
         return [f"https://www.youtube.com/watch?v={vid}" for vid in videos if vid]
 
     @property
     def websites(self):
+        """websites.url"""
         return self._lazy_load('websites.url')
 
     @classmethod
-    def from_igdb(cls,wrapper: IGDBWrapper, name: str, platform: str = "") -> Optional["Game"]:
+    def from_igdb(cls,wrapper: IGDBWrapper, name: str, platform: str = "",load_all: bool = False) -> Optional["Game"]:
         FIELDS = [
             "name","url","platforms.name","cover.url"
         ]
 
-        fields = ",".join(FIELDS)
-        platform_txt = f"&(platforms.name ~ *\"{platform}\"* | platforms.abbreviation ~ *\"{platform}\"*)" if platform else ""
+        params = []
+        if load_all:
+            for _name in dir(Game):
+                attr = getattr(Game,_name)
+                if isinstance(attr, property):
+                    source = attr.fget.__doc__ if attr.fget and attr.fget.__doc__ else _name
+                    params.append(source)
 
+        FIELDS = list(set(FIELDS + params))
+
+        fields = ",".join(FIELDS)
+        print(fields)
+
+        platform_txt = f"&(platforms.name ~ *\"{platform}\"* | platforms.abbreviation ~ *\"{platform}\"*)" if platform else ""
         query = f'fields {fields}; where name ~ *\"{name}\"*{platform_txt}; sort rating desc; limit 100;'
 
         result = wrapper.request('games', query)
@@ -235,28 +292,31 @@ class Game:
             return None
         data = result[0]
 
+        params_dict = {param:safe_get(data,*param.split('.')) for param in params}
+
         return cls(
             wrapper=wrapper,
             id=safe_get(data, "id"),
             name=safe_get(data, "name"),
             url=safe_get(data, "url"),
             platforms=[p["name"] for p in data.get("platforms",[])],
-            cover=("https:" + safe_get(data, "cover", "url",default="")) if safe_get(data, "cover","url",default="") else None
+            cover=("https:" + safe_get(data, "cover", "url",default="")) if safe_get(data, "cover","url",default="") else None,
+            cache=params_dict
         )
 
     def _lazy_load(self,field_name: str):
-        if field_name not in self._cache:
+        if field_name not in self.cache:
             query = f'fields {field_name}; where id = {self.id};'
             result = self.wrapper.request('games', query)
 
             if result:
                 if safe_get(result,*field_name.split('.')):
-                    self._cache[field_name] = safe_get(result,*field_name.split('.'))
+                    self.cache[field_name] = safe_get(result,*field_name.split('.'))
                 else:
-                    self._cache[field_name] = None
+                    self.cache[field_name] = None
             else:
-                self._cache[field_name] = None
-        return self._cache[field_name][0]
+                self.cache[field_name] = None
+        return self.cache[field_name]
 
     def __str__(self):
         return f"ID:{self.id} Game({self.name}) on {', '.join(self.platforms) if self.platforms else 'Unknown platform'}"
